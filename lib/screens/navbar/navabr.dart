cf1f05acc0f621c88b0e4a07b12cd3c4a1bc1d40
UnnikrishnanNP <uknp0371@gmail.com>
2022-01-17 22:25:40 +0530

    crowdless : navbar added

--- lib/screens/navbar/navabr.dart
+++ lib/screens/navbar/navabr.dart
@@ -1,4 +1,5 @@
 import 'package:crowdless/constants/colors.dart';
+import '../../../router/app_router.dart' as route;
 import 'package:flutter/material.dart';
 
 class NavBar extends StatefulWidget {
@@ -11,8 +12,84 @@ class NavBar extends StatefulWidget {
 class _NavBarState extends State<NavBar> {
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
+    return Drawer(
       backgroundColor: primaryLightColor,
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const UserAccountsDrawerHeader(
+            accountName: Text(
+              'Unnikrishnan',
+              style: TextStyle(fontSize: 30),
+            ),
+            accountEmail: Text(
+              'uknp0371@gmail.com',
+              style: TextStyle(fontSize: 15),
+            ),
+            currentAccountPicture: CircleAvatar(
+              backgroundColor: primaryLightColor,
+              child: ClipOval(),
+            ),
+            decoration: BoxDecoration(color: primaryColor),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.person,
+              color: primaryColor,
+            ),
+            title: const Text(
+              'Profile',
+              style: TextStyle(fontSize: 15),
+            ),
+            onTap: () => debugPrint('Profile'),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.settings,
+              color: primaryColor,
+            ),
+            title: const Text(
+              'Settings',
+              style: TextStyle(fontSize: 15),
+            ),
+            onTap: () => debugPrint('Settings'),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.quiz,
+              color: primaryColor,
+            ),
+            title: const Text(
+              'About',
+              style: TextStyle(fontSize: 15),
+            ),
+            onTap: () => debugPrint('About'),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.help,
+              color: primaryColor,
+            ),
+            title: const Text(
+              'Help',
+              style: TextStyle(fontSize: 15),
+            ),
+            onTap: () => debugPrint('Help'),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.logout,
+              color: primaryColor,
+            ),
+            title: const Text(
+              'Logout',
+              style: TextStyle(fontSize: 15),
+            ),
+            onTap: () =>
+                Navigator.pushReplacementNamed(context, route.loginPage),
+          ),
+        ],
+      ),
     );
   }
 }
